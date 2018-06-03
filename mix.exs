1a9d1ed827d19d1b18312629a6599e5b43a9fa34
Jamie Wright <jamie@brilliantfantastic.com>
2018-06-03 14:43:10 -0400

    Initial mix.exs file

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Juvet.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :juvet,
+     version: "0.0.1",
+     elixir: "~> 1.6",
+     name: "Juvet",
+     deps: deps(),
+     docs: docs(),
+     source_url: "https://github.com/juvet/juvet",
+     description: "The message platform framework built in Elixir",
+     package: package()]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp docs do
+    []
+  end
+
+  defp package do
+    %{maintainers: ["Jamie Wright"],
+      licenses: ["MIT"],
+      links: %{
+        "Github": "https://github.com/juvet/juvet",
+        "Documentation": "http://hexdocs.pm/juvet/"}}
+  end
+end
