a5828d9416e8382cb5a6ae66a16321cd2914f9f7
Jamie Wright <jamie@brilliantfantastic.com>
2018-07-05 20:38:39 -0400

    Initial application starts the necessary processes

---
+++ test/juvet_test.exs
@@ -0,0 +1,13 @@
+defmodule Juvet.JuvetTest do
+  use ExUnit.Case, async: true
+
+  describe "Juvet.start/2" do
+    test "starts the pub sub process" do
+      assert Process.whereis(PubSub) |> Process.alive?()
+    end
+
+    test "starts the bot factory supervisor" do
+      assert Process.whereis(Juvet.BotFactorySupervisor) |> Process.alive?()
+    end
+  end
+end
