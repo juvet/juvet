eed816154dec9557daa588dd44476d55553ff6ec
Jamie Wright <jamie@brilliantfantastic.com>
2018-07-07 19:24:54 -0400

    Add documentation to the factories

--- lib/juvet/connection_factory.ex
+++ lib/juvet/connection_factory.ex
@@ -1,28 +1,60 @@
 defmodule Juvet.ConnectionFactory do
   use GenServer
 
+  @moduledoc """
+  A Module for instructing a supervisor on adding and removing
+  connections to third party bot services.
+  """
+
   alias Juvet.ConnectionFactory
 
+  @doc ~S"""
+  Starts a new process for connecting bots in an application.
+
+  Upon initialization, this adds this connection factory as a child
+  process to a connection supervisor. In addition, another supervisor
+  is created as a sibling to this connection factory work process which
+  supervises all of the connections that are added through this factory.
+
+  Returns `{:ok, pid}` where `pid` is the process id of this factory.
+
+  ## Example
+
+  {:ok, pid} = Juvet.ConnectionFactory.start_link(supervisor_pid)
+  """
   def start_link(supervisor) do
     GenServer.start_link(__MODULE__, [supervisor], name: __MODULE__)
   end
 
+  @doc ~S"""
+  Adds a new connection process to the connection supervisor with the
+  `arguments` needed to connect to that `platform`.
+
+  Returns `pid` which is the process id of the connection.
+
+  ## Example
+
+  :ok = Juvet.ConnectionFactory.connect(:slack, %{token: token})
+  """
   def connect(platform, arguments) do
     GenServer.call(ConnectionFactory, {:connect, platform, arguments})
   end
 
   ## Callbacks
 
+  @doc false
   def init([supervisor]) when is_pid(supervisor) do
     init(%{supervisor: supervisor})
   end
 
+  @doc false
   def init(state) do
     send(self(), :start_connection_supervisor)
 
     {:ok, state}
   end
 
+  @doc false
   def handle_call(
         {:connect, _platform, arguments},
         _from,
@@ -41,6 +73,7 @@ defmodule Juvet.ConnectionFactory do
     {:reply, pid, state}
   end
 
+  @doc false
   def handle_info(
         :start_connection_supervisor,
         %{supervisor: supervisor} = state
@@ -52,6 +85,7 @@ defmodule Juvet.ConnectionFactory do
      Map.merge(state, %{connection_supervisor: connection_supervisor})}
   end
 
+  @doc false
   defp connection_supervisor_spec() do
     Supervisor.Spec.supervisor(
       Juvet.ConnectionSupervisor,
--- lib/juvet/connection_factory_supervisor.ex
+++ lib/juvet/connection_factory_supervisor.ex
@@ -1,10 +1,28 @@
 defmodule Juvet.ConnectionFactorySupervisor do
   use Supervisor
 
+  @moduledoc """
+  A supervisor that supervises a worker `ConnectionFactory` process and
+  another supervisor `ConnectionSupervisor` which in turn, supervises all
+  of the connection processes.
+  """
+
+  @doc ~S"""
+  Starts a new connection factory supervisor
+
+  Returns `{:ok, pid}` where `pid` is the process id of this supervisor.
+
+  ## Example
+
+  {:ok, pid} = Juvet.ConnectionFactorySupervisor.start_link()
+  """
   def start_link() do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  ## Callbacks
+
+  @doc false
   def init(_args) do
     children = [
       worker(Juvet.ConnectionFactory, [self()])
