b0a7ec87e4e3ca3f550a4b70ee83cf0d8b44185f
Jamie Wright <jamie@brilliantfantastic.com>
2018-08-20 20:39:59 -0400

    Documentation for the IM.open method

--- lib/juvet/slack_api/im.ex
+++ lib/juvet/slack_api/im.ex
@@ -1,6 +1,26 @@
 defmodule Juvet.SlackAPI.IM do
   alias Juvet.SlackAPI
 
+  @moduledoc """
+  A wrapper around the im methods on the Slack API.
+  """
+
+  @doc ~S"""
+  Requests a new IM channel between the requestor and the user specified.
+
+  Returns a map of the Slack response.
+
+  ## Example
+
+  %{
+    ok: true,
+    no_op: true,
+    channel: {
+      id: "D123456"
+    }
+  } = Juvet.SlackAPI.IM.open(%{token: token, user: user})
+  """
+
   def open(options \\ %{}) do
     SlackAPI.request("im.open", options)
     |> SlackAPI.render_response()
