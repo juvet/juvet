000d838f85de58a5709d9dcb8aba56aaaf3552cb
Jamie Wright <jamie@brilliantfantastic.com>
2020-10-20 00:20:07 +0000

    Pass config to the superintendent

--- test/juvet/bot_factory_test.exs
+++ test/juvet/bot_factory_test.exs
@@ -10,9 +10,8 @@ defmodule Juvet.BotFactoryTest do
   end
 
   describe "Juvet.BotFactory.start_link/1" do
-    @tag :skip
     test "starts the superintendent", %{config: config} do
-      Juvet.BotFactory.start_link(config)
+      start_supervised!({Juvet.BotFactory, config})
 
       assert Process.whereis(Juvet.Superintendent) |> Process.alive?()
     end
@@ -20,24 +19,16 @@ defmodule Juvet.BotFactoryTest do
     test "starts the bot supervisor if the configuration is valid", %{
       config: config
     } do
-      {:ok, _pid} = Juvet.BotFactory.start_link(config)
-
-      [{id, pid, type, [modules]} | _] =
-        Supervisor.which_children(Juvet.BotFactory)
-
-      IO.inspect([modules])
+      start_supervised!({Juvet.BotFactory, config})
 
       %{bot_supervisor: supervisor_pid} = Juvet.Superintendent.get_state()
 
       assert Process.alive?(supervisor_pid)
     end
 
-    @tag :skip
     test "does not start the bot supervisor via the superintendent if the configuration is not valid",
          %{config: config} do
-      # Application.put_env(:juvet, :bot, nil)
-
-      Juvet.BotFactory.start_link(config)
+      start_supervised!({Juvet.BotFactory, Keyword.merge(config, bot: nil)})
 
       refute Map.has_key?(Juvet.Superintendent.get_state(), :bot_supervisor)
     end
@@ -45,8 +36,7 @@ defmodule Juvet.BotFactoryTest do
 
   describe "Juvet.BotFactory.create/2" do
     setup context do
-      # TODO: May need to check if it's already started so first test does not fail
-      Juvet.BotFactory.start_link(context.config)
+      start_supervised!({Juvet.BotFactory, context.config})
 
       :ok
     end
