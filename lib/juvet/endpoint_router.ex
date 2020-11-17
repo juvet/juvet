b24310975bd5e5cd412c70575d818f6a6bd8ef62
Jamie Wright <jamie@brilliantfantastic.com>
2020-11-16 23:59:22 -0500

    Moved the slack router routes to it's own module

--- lib/juvet/endpoint_router.ex
+++ lib/juvet/endpoint_router.ex
@@ -1,33 +1,11 @@
 defmodule Juvet.EndpointRouter do
   use Plug.Router
+  use Juvet.SlackEndpointRouter, config: Application.get_all_env(:juvet)
 
   plug(Plug.Logger)
   plug(:match)
   plug(Plug.Parsers, parsers: [:json], json_decoder: Poison)
   plug(:dispatch)
-
-  @config Application.get_all_env(:juvet)
-
-  if Juvet.Config.slack_configured?(@config) do
-    defaults = %{actions_endpoint: nil, events_endpoint: nil}
-
-    %{actions_endpoint: actions_endpoint, events_endpoint: events_endpoint} =
-      Map.merge(defaults, Juvet.Config.slack(@config))
-
-    if events_endpoint,
-      do:
-        post(events_endpoint,
-          to: Juvet.SlackEventsEndpointRouter,
-          init_opts: @config
-        )
-
-    if actions_endpoint,
-      do:
-        post(actions_endpoint,
-          to: Juvet.SlackActionsEndpointRouter,
-          init_opts: @config
-        )
-  end
 
   # TODO: Make this response configurable like in Phoenix
   match(_, do: send_resp(conn, 404, "Oh no! This route is not handled in Juvet"))
