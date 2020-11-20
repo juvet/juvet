78f2809df0d67c03009f61019f623da275f73243
Jamie Wright <jamie@brilliantfantastic.com>
2020-11-19 19:12:05 -0500

    WIP: Start adding documentation

--- lib/juvet.ex
+++ lib/juvet.ex
@@ -1,10 +1,16 @@
 defmodule Juvet do
+  @moduledoc """
+  """
+
   use Application
 
+  @doc false
   def start(_types, _args) do
     Juvet.BotFactory.start_link(Application.get_all_env(:juvet))
   end
 
+  @doc """
+  """
   def create_bot(name) do
     Juvet.BotFactory.create(name)
   end
