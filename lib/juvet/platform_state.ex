cac8c5d3a5b21d07da9666e99040bb9e193a3c7f
Jamie Wright <jamie@brilliantfantastic.com>
2021-01-22 20:50:41 -0500

    Add Platform state to return the team information

--- lib/juvet/platform_state.ex
+++ lib/juvet/platform_state.ex
@@ -1,3 +1,27 @@
 defmodule Juvet.PlatformState do
   defstruct teams: []
+
+  def add_team(state, team) do
+    case has_team?(state, team) do
+      false ->
+        teams = state.teams
+        %{state | teams: teams ++ [{team, %Juvet.TeamState{}}]}
+
+      true ->
+        state
+    end
+  end
+
+  def has_team?(state, team) do
+    Enum.any?(state.teams, &find(&1, team))
+  end
+
+  def team(state, team) do
+    case Enum.find(state.teams, &find(&1, team)) do
+      {_team, team_state} -> team_state
+      nil -> nil
+    end
+  end
+
+  defp find(team, id), do: Kernel.elem(team, 0) == id
 end
---
+++ test/juvet/platform_state_test.exs
@@ -0,0 +1,46 @@
+defmodule Juvet.PlatformStateTest do
+  use ExUnit.Case
+
+  setup_all do
+    [state: %Juvet.PlatformState{}]
+  end
+
+  describe "Juvet.PlatformState.add_team/2" do
+    test "adds the team to the list of teams", %{state: state} do
+      state = Juvet.PlatformState.add_team(state, "T1234")
+
+      assert state.teams == [{"T1234", %Juvet.TeamState{}}]
+    end
+
+    test "does not duplicate the team if it already exists", %{state: state} do
+      state = Juvet.PlatformState.add_team(state, "T1234")
+      state = Juvet.PlatformState.add_team(state, "T1234")
+
+      assert state.teams == [{"T1234", %Juvet.TeamState{}}]
+    end
+  end
+
+  describe "Juvet.PlatformState.has_team?/2" do
+    test "returns false if the team does not exist", %{state: state} do
+      refute Juvet.PlatformState.has_team?(state, "T1234")
+    end
+
+    test "returns true if the team does exist", %{state: state} do
+      state = Juvet.PlatformState.add_team(state, "T1234")
+
+      assert Juvet.PlatformState.has_team?(state, "T1234")
+    end
+  end
+
+  describe "Juvet.PlatformState.team/2" do
+    test "returns the state for the specified team", %{state: state} do
+      state = Juvet.PlatformState.add_team(state, "T1234")
+
+      assert %Juvet.TeamState{} = Juvet.PlatformState.team(state, "T1234")
+    end
+
+    test "returns nil if the team does not exist", %{state: state} do
+      assert Juvet.PlatformState.team(state, "T1234") == nil
+    end
+  end
+end
