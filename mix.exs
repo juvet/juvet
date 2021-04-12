ffa07946d480fd1d34fb4ffc60ed179c7758752a
Jamie Wright <jamie@brilliantfantastic.com>
2021-04-12 11:38:33 -0400

    Move the mix task to record vcr responses to test

--- mix.exs
+++ mix.exs
@@ -41,8 +41,7 @@ defmodule Juvet.Mixfile do
       {:plug_cowboy, "~> 2.0"},
       {:poison, "~> 3.1"},
       {:websockex, "~> 0.4.0"},
-      # TODO: {:exvcr, "~> 0.10", only: [:dev, :test]} but need this now because of Mix task `record`
-      {:exvcr, "~> 0.10"}
+      {:exvcr, "~> 0.10", only: [:dev, :test]}
     ]
   end
 
