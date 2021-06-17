386a4fa012904e4fd712219506ca10b74c12ef9f
Jamie Wright <jamie@brilliantfantastic.com>
2021-06-17 07:15:28 -0400

    ActionRunner handles UndefinedFunctionErrors

--- lib/juvet/middleware/action_runner.ex
+++ lib/juvet/middleware/action_runner.ex
@@ -3,9 +3,14 @@ defmodule Juvet.Middleware.ActionRunner do
     m = elem(action, 0)
     f = elem(action, 1)
 
-    apply(m, f, [context])
-
-    {:ok, context}
+    try do
+      apply(m, f, [context])
+    rescue
+      UndefinedFunctionError -> {:error, "`#{m}.#{f}/1` is not defined"}
+    else
+      _ ->
+        {:ok, context}
+    end
   end
 
   def call(_context), do: {:error, "`action` missing in the `context`"}
--- test/juvet/middleware/action_runner_test.exs
+++ test/juvet/middleware/action_runner_test.exs
@@ -24,6 +24,13 @@ defmodule Juvet.Middleware.ActionRunnerTest do
       assert result == {:error, "`action` missing in the `context`"}
     end
 
+    test "returns an error if the controller action could not be called" do
+      result =
+        Juvet.Middleware.ActionRunner.call(%{action: {:TestController, :action}})
+
+      assert result == {:error, "`TestController.action/1` is not defined"}
+    end
+
     test "calls the controller module and action with the context", %{
       context: context
     } do
