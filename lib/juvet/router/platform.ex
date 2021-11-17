cae851ecd2a9f970fd3afb4ef2a1921c085e90ba
Jamie Wright <jamie@brilliantfantastic.com>
2021-11-16 23:12:05 -0500

    Implement Router.Platform.put_route/3

--- lib/juvet/router/platform.ex
+++ lib/juvet/router/platform.ex
@@ -7,6 +7,17 @@ defmodule Juvet.Router.Platform do
     %__MODULE__{platform: platform}
   end
 
+  def put_route(platform, route, options \\ %{}) do
+    case validate_route(platform, route, options) do
+      {:ok, route} ->
+        routes = platform.routes
+        {:ok, %{platform | routes: routes ++ [route]}}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
   def validate_route(%__MODULE__{platform: platform}, route, options \\ %{}) do
     PlatformFactory.new(platform)
     |> PlatformFactory.validate_route(route, options)
--- test/juvet/router/platform_test.exs
+++ test/juvet/router/platform_test.exs
@@ -1,6 +1,33 @@
 defmodule Juvet.Router.PlatformTest do
   use ExUnit.Case, async: true
 
+  describe "Juvet.Router.Platform.put_route/3" do
+    setup do
+      platform = Juvet.Router.Platform.new(:slack)
+      route = Juvet.Router.Route.new(:command, "/test", to: "controller#action")
+
+      [platform: platform, route: route]
+    end
+
+    test "returns an ok tuple with the route added to the returning platform",
+         %{platform: platform, route: route} do
+      assert {:ok, platform} = Juvet.Router.Platform.put_route(platform, route)
+      assert Enum.count(platform.routes) == 1
+      assert List.first(platform.routes).type == :command
+    end
+
+    test "returns an error tuple with the error from the returning platform", %{
+      platform: platform
+    } do
+      error_route = %Juvet.Router.Route{type: :blah}
+
+      assert {:error, error} =
+               Juvet.Router.Platform.put_route(platform, error_route)
+
+      assert error == {:routing_error, [route: error_route, options: %{}]}
+    end
+  end
+
   describe "Juvet.Router.Platform.validate_route/3" do
     setup do
       platform = Juvet.Router.Platform.new(:slack)
