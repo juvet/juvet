604604e0933c474af19866c6181b1279f2e2b00f
Jamie Wright <jamie@brilliantfantastic.com>
2022-01-07 12:22:57 -0500

    Get the Plug.Test.Conn working with headers

--- test/support/plug_helpers.ex
+++ test/support/plug_helpers.ex
@@ -7,21 +7,35 @@ defmodule Juvet.PlugHelpers do
     quote do
       use Plug.Test
 
+      alias Plug.Conn
+
       defp request!(method, path, params_or_body \\ nil, headers \\ nil) do
-        conn(method, path, params_or_body)
-        |> put_request_headers(headers)
+        build_conn(method, path, params_or_body, headers)
         |> Juvet.Plug.call(Juvet.Plug.init([]))
+        |> from_set_to_sent()
       end
 
-      defp put_request_headers(conn, nil), do: conn
-
-      defp put_request_headers(conn, headers) do
-        Enum.each(headers, fn {key, value} ->
-          conn = conn |> put_req_header(to_string(key), value)
-        end)
-
-        conn
+      defp build_conn(method, path, params_or_body, nil) do
+        Plug.Adapters.Test.Conn.conn(
+          %Conn{},
+          method,
+          path,
+          params_or_body
+        )
       end
+
+      defp build_conn(method, path, params_or_body, headers) do
+        Plug.Adapters.Test.Conn.conn(
+          %Conn{req_headers: headers},
+          method,
+          path,
+          params_or_body
+        )
+      end
+
+      defp from_set_to_sent(%Conn{state: :set} = conn), do: Conn.send_resp(conn)
+
+      defp from_set_to_sent(conn), do: conn
     end
   end
 end
