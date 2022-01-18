e3d06e15ea54191a64d5539a4a08d4851bc8143f
Jamie Wright <jamie@brilliantfantastic.com>
2022-01-18 00:25:06 -0500

    Helper module to help find routes based on a request

---
+++ lib/juvet/router/route_finder.ex
@@ -0,0 +1,23 @@
+defmodule Juvet.Router.RouteFinder do
+  alias Juvet.Router.Platform
+
+  def find(platforms, request) do
+    route =
+      Enum.map(platforms, fn platform ->
+        case find_route(platform, request) do
+          {:error, _error} -> nil
+          {:ok, route} -> route
+        end
+      end)
+      |> List.first()
+
+    case route do
+      nil -> {:error, :not_found}
+      route -> {:ok, route}
+    end
+  end
+
+  def find_route(platform, request) do
+    Platform.find_route(platform, request)
+  end
+end
---
+++ test/juvet/router/route_finder_test.exs
@@ -0,0 +1,42 @@
+defmodule Juvet.Router.RouteFinderTest do
+  use ExUnit.Case, async: true
+
+  alias Juvet.Router.RouteFinder
+
+  describe "find/2" do
+    setup do
+      route = Juvet.Router.Route.new(:command, "/test", to: "controller#action")
+
+      {:ok, platform} =
+        Juvet.Router.Platform.new(:slack)
+        |> Juvet.Router.Platform.put_route(route)
+
+      request = Juvet.Router.Request.new(%{params: %{"command" => "test"}})
+      request = %{request | platform: :slack, verified?: true}
+
+      [platforms: [platform], request: request]
+    end
+
+    test "returns an ok tuple with the route if it was found", %{
+      platforms: platforms,
+      request: request
+    } do
+      assert {:ok, route} = RouteFinder.find(platforms, request)
+      assert route.type == :command
+      assert route.route == "/test"
+      assert route.options == [to: "controller#action"]
+    end
+
+    test "returns an error tuple with an error if the route was not found", %{
+      platforms: platforms,
+      request: request
+    } do
+      request = %{
+        request
+        | params: Map.merge(request.params, %{"command" => "blah"})
+      }
+
+      assert {:error, :not_found} = RouteFinder.find(platforms, request)
+    end
+  end
+end
