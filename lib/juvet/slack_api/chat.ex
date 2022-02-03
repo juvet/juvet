9a695d963e025f5997370673c777b735bb6796b4
Jamie Wright <jamie@brilliantfantastic.com>
2022-02-03 16:13:50 -0500

    Add chat.update endpoint

--- lib/juvet/slack_api/chat.ex
+++ lib/juvet/slack_api/chat.ex
@@ -19,9 +19,28 @@ defmodule Juvet.SlackAPI.Chat do
     }
   } = Juvet.SlackAPI.Chat.post_message(%{token: token, channel: channel, text: text})
   """
-
   def post_message(options \\ %{}) do
     SlackAPI.make_request("chat.postMessage", options)
+    |> SlackAPI.render_response()
+  end
+
+  @doc """
+  Updates an existing message based on the timestamp and sends it to the channel specified.
+
+  Returns a map of the Slack response.
+
+  ## Example
+
+  %{
+    ok: true,
+    ts: "12345678.19872",
+    message: {
+      text: "This has been updated!"
+    }
+  } = Juvet.SlackAPI.Chat.update(%{token: token, channel: channel, text: text, ts: timestamp})
+  """
+  def update(options \\ %{}) do
+    SlackAPI.make_request("chat.update", options)
     |> SlackAPI.render_response()
   end
 end
--- test/juvet/slack_api/chat_test.exs
+++ test/juvet/slack_api/chat_test.exs
@@ -9,7 +9,8 @@ defmodule Juvet.SlackAPI.ChatTest do
   end
 
   setup do
-    {:ok, channel: "CHANNEL1", text: "Hello from Juvet", token: "TOKEN"}
+    {:ok,
+     channel: "CHANNEL1", text: "Hello from Juvet", timestamp: "1643854606.765529", token: "TOKEN"}
   end
 
   describe "SlackAPI.Chat.post_message/1" do
@@ -37,6 +38,46 @@ defmodule Juvet.SlackAPI.ChatTest do
                    channel: channel,
                    text: text,
                    token: token
+                 })
+
+        assert response[:error] == "invalid_auth"
+      end
+    end
+  end
+
+  describe "SlackAPI.Chat.update/1" do
+    test "returns the message", %{
+      channel: channel,
+      text: text,
+      timestamp: timestamp,
+      token: token
+    } do
+      use_cassette "chat/update/successful" do
+        assert {:ok, response} =
+                 SlackAPI.Chat.update(%{
+                   channel: channel,
+                   text: text,
+                   token: token,
+                   ts: timestamp
+                 })
+
+        assert response[:message][:text] == "Hello from Juvet"
+      end
+    end
+
+    test "returns an error from an unsuccessful API call", %{
+      channel: channel,
+      text: text,
+      timestamp: timestamp,
+      token: token
+    } do
+      use_cassette "chat/update/invalid_auth" do
+        assert {:error, response} =
+                 SlackAPI.Chat.update(%{
+                   channel: channel,
+                   text: text,
+                   token: token,
+                   ts: timestamp
                  })
 
         assert response[:error] == "invalid_auth"
