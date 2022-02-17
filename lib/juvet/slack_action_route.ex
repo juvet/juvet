15169b27adc68fe2786647e44802bcdeaa44a791
Jamie Wright <jamie@brilliantfantastic.com>
2022-02-17 12:13:10 -0500

    Fix credo warnings

--- lib/juvet/slack_action_route.ex
+++ lib/juvet/slack_action_route.ex
@@ -3,11 +3,13 @@ defmodule Juvet.SlackActionRoute do
   Plug to handle any action from Slack.
   """
 
+  alias Juvet.Router.Conn
+
   @doc false
   def init(opts), do: opts
 
   @doc """
   Handles web requests targeted for the Slack actions API endpoint.
   """
-  def call(conn, _opts), do: Juvet.Router.Conn.run(conn)
+  def call(conn, _opts), do: Conn.run(conn)
 end
--- lib/juvet/slack_command_route.ex
+++ lib/juvet/slack_command_route.ex
@@ -3,11 +3,13 @@ defmodule Juvet.SlackCommandRoute do
   Plug to handle any command from Slack.
   """
 
+  alias Juvet.Router.Conn
+
   @doc false
   def init(opts), do: opts
 
   @doc """
   Handles web requests targeted for the Slack commands API endpoint.
   """
-  def call(conn, _opts), do: Juvet.Router.Conn.run(conn)
+  def call(conn, _opts), do: Conn.run(conn)
 end
