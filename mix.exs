38a67a328dc07afdeeed0dce7a1912ea38e6113d
Jamie Wright <jamie@brilliantfantastic.com>
2022-10-10 10:51:54 -0400

    Bump from 0.0.3 -> 0.1.0 since we broke the SlackAPI

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Juvet.Mixfile do
   def project do
     [
       app: :juvet,
-      version: "0.0.3",
+      version: "0.1.0",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "Juvet",
