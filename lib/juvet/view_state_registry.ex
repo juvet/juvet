8603cc32178f209ec6c662d4b52c43805ddcdbb5
Jamie Wright <jamie@brilliantfantastic.com>
2022-12-01 09:03:47 -0500

    ViewStateRegistry to convert keys to pids

---
+++ lib/juvet/view_state_registry.ex
@@ -0,0 +1,73 @@
+defmodule Juvet.ViewStateRegistry do
+  @moduledoc """
+  A process to keep track of what view state keys are registered. It is used as a Registry
+  for other processes for the view state in order to exchange a list of tuples for a name.
+  """
+
+  use GenServer
+
+  @name :view_state_registry
+
+  @spec start_link(any()) :: {:ok, pid()} | {:error, any()}
+  def start_link(_), do: GenServer.start_link(__MODULE__, [], name: @name)
+
+  @spec start_link() :: {:ok, pid()} | {:error, any()}
+  def start_link, do: GenServer.start_link(__MODULE__, [], name: @name)
+
+  @spec register_name(tuple() | binary(), pid()) :: :ok | :already_registered
+  def register_name(name, pid), do: GenServer.call(@name, {:register_name, name, pid})
+
+  @spec send(tuple() | binary(), any()) :: pid() | {:badarg, {tuple() | binary(), any()}}
+  def send(name, message) do
+    case whereis_name(name) do
+      :undefined ->
+        {:badarg, {name, message}}
+
+      pid ->
+        Kernel.send(pid, message)
+        pid
+    end
+  end
+
+  @spec unregister_name(tuple() | binary()) :: :ok
+  def unregister_name(name), do: GenServer.cast(@name, {:unregister_name, name})
+
+  @spec whereis_name(tuple() | binary()) :: pid() | :undefined
+  def whereis_name(name), do: GenServer.call(@name, {:whereis_name, name})
+
+  # Callbacks
+  def init(_) do
+    {:ok, Map.new()}
+  end
+
+  def handle_call({:register_name, name, pid}, _from, state) do
+    case state |> find_by_name(name) do
+      :undefined ->
+        Process.monitor(pid)
+        {:reply, :ok, state |> Map.put_new(name, pid)}
+
+      _ ->
+        {:reply, :already_registered, state}
+    end
+  end
+
+  def handle_call({:whereis_name, name}, _from, state) do
+    {:reply, state |> find_by_name(name), state}
+  end
+
+  def handle_cast({:unregister_name, name}, state) do
+    {:noreply, state |> Map.delete(name)}
+  end
+
+  def handle_info({:DOWN, _, :process, pid, _}, state) do
+    {:noreply, state |> remove_by_pid(pid)}
+  end
+
+  defp find_by_name(state, name), do: state |> Map.get(name, :undefined)
+
+  defp remove_by_pid(state, pid_to_remove) do
+    state
+    |> Enum.reject(fn {_key, pid} -> pid == pid_to_remove end)
+    |> Enum.into(%{})
+  end
+end
---
+++ test/juvet/view_state_registry_test.exs
@@ -0,0 +1,92 @@
+defmodule Juvet.ViewStateRegistryTest do
+  use ExUnit.Case, async: false
+
+  alias Juvet.ViewStateRegistry
+
+  describe "start_link" do
+    test "returns the pid" do
+      assert {:ok, pid} = ViewStateRegistry.start_link()
+      assert is_pid(pid)
+    end
+  end
+
+  describe "register_name/2" do
+    setup do
+      {:ok, pid} = ViewStateRegistry.start_link()
+
+      [pid: pid]
+    end
+
+    test "registers the name with the pid for lookup" do
+      :ok = ViewStateRegistry.register_name({:this, :is, :a, :key}, self())
+
+      pid = ViewStateRegistry.whereis_name({:this, :is, :a, :key})
+
+      assert pid == self()
+    end
+
+    test "does not register a name again that already exists" do
+      assert :ok = ViewStateRegistry.register_name({:this, :is, :a, :key}, self())
+
+      assert :already_registered =
+               ViewStateRegistry.register_name(
+                 {:this, :is, :a, :key},
+                 spawn(fn -> nil end)
+               )
+    end
+
+    test "removes the name when the process dies" do
+      name = {:another, :key}
+
+      pid = spawn(fn -> nil end)
+
+      :ok = ViewStateRegistry.register_name(name, pid)
+
+      pid |> Process.exit(:kill)
+
+      assert :undefined = ViewStateRegistry.whereis_name(name)
+    end
+  end
+
+  describe "send/2" do
+    setup do
+      {:ok, pid} = ViewStateRegistry.start_link()
+
+      [pid: pid]
+    end
+
+    test "sends the process a message based on the name" do
+      name = {:send, :me, :a, :message}
+      :ok = ViewStateRegistry.register_name(name, self())
+
+      pid = ViewStateRegistry.send(name, :message)
+
+      assert pid == self()
+      assert_received :message
+    end
+
+    test "does not send a message if the process cannot be found" do
+      name = {:send, :me, :a, :message}
+      :ok = ViewStateRegistry.register_name(name, self())
+      :ok = ViewStateRegistry.unregister_name(name)
+
+      assert {:badarg, {^name, :message}} = ViewStateRegistry.send(name, :message)
+    end
+  end
+
+  describe "unregister_name/1" do
+    setup do
+      {:ok, pid} = ViewStateRegistry.start_link()
+
+      [pid: pid]
+    end
+
+    test "removes the name from lookup" do
+      name = {:this, :key, :works}
+      :ok = ViewStateRegistry.register_name(name, self())
+
+      assert :ok = ViewStateRegistry.unregister_name(name)
+      assert :undefined = ViewStateRegistry.whereis_name(name)
+    end
+  end
+end
