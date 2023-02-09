fe652d54d1098925652e995b58859e124899d8c2
Jamie Wright <jamie@brilliantfantastic.com>
2023-02-09 14:39:21 -0500

    Include the team id in the normalized parameters

--- lib/juvet/router/request_param_normalizer.ex
+++ lib/juvet/router/request_param_normalizer.ex
@@ -17,10 +17,13 @@ defmodule Juvet.Router.RequestParamNormalizer do
 
     def normalize(%Request{raw_params: _raw_params} = request), do: request
 
+    defp team_id(%{"team" => %{"id" => team_id}}), do: team_id
+    defp team_id(_payload), do: nil
+
     defp user_id(%{"user" => %{"id" => user_id}}), do: user_id
     defp user_id(_payload), do: nil
 
-    defp normalized_params(payload), do: %{user_id: user_id(payload)}
+    defp normalized_params(payload), do: %{team_id: team_id(payload), user_id: user_id(payload)}
   end
 
   def normalize(%Request{platform: :slack} = request),
