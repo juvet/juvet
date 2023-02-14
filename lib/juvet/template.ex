f60e6de28e8ab8b0cb4e5b86416514b9d90a8981
Jamie Wright <jamie@brilliantfantastic.com>
2023-02-13 21:58:28 -0500

    Add moduledoc

--- lib/juvet/template.ex
+++ lib/juvet/template.ex
@@ -1,4 +1,8 @@
 defmodule Juvet.Template do
+  @moduledoc """
+  Represents a single message that is compiled into Elixir code and is sent to different platform endpoints.
+  """
+
   def send_message(view, template, assigns) do
   end
 end
