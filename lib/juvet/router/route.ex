6a6da7c47235e43238a3713b960ff22c940609f5
Jamie Wright <jamie@brilliantfantastic.com>
2023-03-04 00:05:24 -0500

    Allow a route to only require a type since url_verification does not have a route name:

--- lib/juvet/router/route.ex
+++ lib/juvet/router/route.ex
@@ -5,13 +5,13 @@ defmodule Juvet.Router.Route do
 
   @type t :: %__MODULE__{
           type: atom(),
-          route: String.t(),
+          route: String.t() | nil,
           options: keyword()
         }
   defstruct type: nil, route: nil, options: []
 
   @spec new(atom(), String.t(), keyword()) :: Juvet.Router.Route.t()
-  def new(type, route, options \\ []) do
+  def new(type, route \\ nil, options \\ []) do
     %__MODULE__{type: type, route: route, options: options}
   end
 
