38103abb4cf0619ef05172419e32c822b50da75b
Jamie Wright <jamie@brilliantfantastic.com>
2023-07-12 18:21:50 -0400

    Add describe blocks

--- test/juvet/router/request_identifier_test.exs
+++ test/juvet/router/request_identifier_test.exs
@@ -7,76 +7,80 @@ defmodule Juvet.Router.RequestIdentifierTest do
     [configuration: Juvet.configuration()]
   end
 
-  test "platform/2 returns slack for a Slack request with a header", %{
-    configuration: configuration
-  } do
-    request =
-      Request.new(%{
-        req_headers: [{"x-slack-signature", "blah"}]
-      })
+  describe "platform/2" do
+    test "returns slack for a Slack request with a header", %{
+      configuration: configuration
+    } do
+      request =
+        Request.new(%{
+          req_headers: [{"x-slack-signature", "blah"}]
+        })
 
-    assert :slack = RequestIdentifier.platform(request, configuration)
+      assert :slack = RequestIdentifier.platform(request, configuration)
+    end
+
+    test "returns slack for a slack oauth request", %{configuration: configuration} do
+      request =
+        Request.new(%{
+          method: "GET",
+          request_path: "/auth/slack"
+        })
+
+      assert :slack = RequestIdentifier.platform(request, configuration)
+    end
+
+    test "returns unknown if the oauth request does not match any request in config", %{
+      configuration: configuration
+    } do
+      request =
+        Request.new(%{
+          method: "GET",
+          request_path: "/auth/blah"
+        })
+
+      assert :unknown = RequestIdentifier.platform(request, configuration)
+    end
+
+    test "returns unknown if the request cannot be identified", %{
+      configuration: configuration
+    } do
+      request = Request.new(%{platform: :slack})
+
+      assert :unknown = RequestIdentifier.platform(request, configuration)
+    end
   end
 
-  test "platform/2 returns slack for a slack oauth request", %{configuration: configuration} do
-    request =
-      Request.new(%{
-        method: "GET",
-        request_path: "/auth/slack"
-      })
+  describe "oauth/2" do
+    test "oauth?/2 returns true for a slack oauth request", %{configuration: configuration} do
+      request =
+        Request.new(%{
+          method: "GET",
+          request_path: "/auth/slack?code=blah"
+        })
 
-    assert :slack = RequestIdentifier.platform(request, configuration)
-  end
+      assert RequestIdentifier.oauth?(%{request | platform: :slack}, configuration)
+    end
 
-  test "platform/2 returns unknown if the oauth request does not match any request in config", %{
-    configuration: configuration
-  } do
-    request =
-      Request.new(%{
-        method: "GET",
-        request_path: "/auth/blah"
-      })
+    test "returns false for a slack oauth request with a different request path", %{
+      configuration: configuration
+    } do
+      request =
+        Request.new(%{
+          method: "GET",
+          request_path: "/oauth/slack"
+        })
 
-    assert :unknown = RequestIdentifier.platform(request, configuration)
-  end
+      refute RequestIdentifier.oauth?(%{request | platform: :slack}, configuration)
+    end
 
-  test "platform/2 returns unknown if the request cannot be identified", %{
-    configuration: configuration
-  } do
-    request = Request.new(%{platform: :slack})
+    test "returns false for an unknown oauth request", %{configuration: configuration} do
+      request =
+        Request.new(%{
+          method: "GET",
+          request_path: "/oauth/slack"
+        })
 
-    assert :unknown = RequestIdentifier.platform(request, configuration)
-  end
-
-  test "oauth?/2 returns true for a slack oauth request", %{configuration: configuration} do
-    request =
-      Request.new(%{
-        method: "GET",
-        request_path: "/auth/slack?code=blah"
-      })
-
-    assert RequestIdentifier.oauth?(%{request | platform: :slack}, configuration)
-  end
-
-  test "oauth?/2 returns false for a slack oauth request with a different request path", %{
-    configuration: configuration
-  } do
-    request =
-      Request.new(%{
-        method: "GET",
-        request_path: "/oauth/slack"
-      })
-
-    refute RequestIdentifier.oauth?(%{request | platform: :slack}, configuration)
-  end
-
-  test "oauth?/2 returns false for an unknown oauth request", %{configuration: configuration} do
-    request =
-      Request.new(%{
-        method: "GET",
-        request_path: "/oauth/slack"
-      })
-
-    refute RequestIdentifier.oauth?(request, configuration)
+      refute RequestIdentifier.oauth?(request, configuration)
+    end
   end
 end
