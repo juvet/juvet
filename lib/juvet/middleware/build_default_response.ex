365acc17f68353dca6b9a06c558acce0e8619047
Jamie Wright <jamie@brilliantfantastic.com>
2023-07-12 20:32:59 -0400

    Build a redirect response if it is an oauth request

--- lib/juvet/middleware/build_default_response.ex
+++ lib/juvet/middleware/build_default_response.ex
@@ -4,13 +4,24 @@ defmodule Juvet.Middleware.BuildDefaultResponse do
   contained within the context.
   """
 
-  alias Juvet.Router.Response
+  alias Juvet.Router.{OAuthRouter, RequestIdentifier, Response}
 
   @spec call(map()) :: {:ok, map()}
   def call(%{request: _request} = context),
-    do: {:ok, Map.put_new(context, :response, default_response())}
+    do: {:ok, Map.put_new(context, :response, default_response(context))}
 
   def call(context), do: {:ok, context}
 
-  defp default_response, do: Response.new(status: 200)
+  defp default_response(%{configuration: configuration, request: request, route: route}) do
+    if RequestIdentifier.oauth?(request, configuration),
+      do: default_oauth_response(request, route, configuration),
+      else: Response.new(status: 200)
+  end
+
+  defp default_oauth_response(%{platform: platform}, %{route: phase}, configuration) do
+    Response.new(
+      body: OAuthRouter.url_for(platform, phase, configuration),
+      status: 302
+    )
+  end
 end
