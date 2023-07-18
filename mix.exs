e3b5b6686e6014a8546d930dd21df23d6d252b9d
Jamie Wright <jamie@brilliantfantastic.com>
2023-07-17 20:46:22 -0400

    Bump version from 0.5.4 -> 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Juvet.Mixfile do
   def project do
     [
       app: :juvet,
-      version: "0.5.4",
+      version: "0.6.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "Juvet",
