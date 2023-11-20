a43e9ae9e2b8106e32b9b3bfdbb6b6702b767800
Jamie Wright <jamie@brilliantfantastic.com>
2023-11-20 18:23:34 -0500

    WIP: Added reactions methods to the SlackAPI

---
+++ lib/juvet/slack_api/reactions.ex
@@ -0,0 +1,28 @@
+defmodule Juvet.SlackAPI.Reactions do
+  @moduledoc """
+  A wrapper around the reactions methods on the Slack API.
+  """
+
+  alias Juvet.SlackAPI
+
+  def add(options \\ %{}), do: request_and_render("reactions.add", options)
+
+  def get(options \\ %{}), do: request_and_render("reactions.get", options)
+
+  def list(options \\ %{}), do: request_and_render("reactions.list", options)
+
+  def remove(options \\ %{}), do: request_and_render("reactions.remove", options)
+
+  defp request_and_render(method, options) do
+    options = options |> transform_options
+
+    SlackAPI.make_request(method, options)
+    |> SlackAPI.render_response()
+  end
+
+  defp transform_options(options) do
+    options
+    |> Enum.filter(fn {_key, value} -> !is_nil(value) end)
+    |> Enum.into(%{})
+  end
+end
